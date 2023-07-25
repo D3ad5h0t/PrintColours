379a23d0d3f79ec09e0c273740f584cab429b6f0
D3ad5h0t <alexgl007@mail.ru>
2023-07-26 02:41:22 +1000

    print messages in green

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("==> I'm printing in green");
+Console.ResetColor();
