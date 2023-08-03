1571468d825c93ffcab5dcc8e3653c1410bbcff3
D3ad5h0t <alexgl007@mail.ru>
2023-08-04 01:04:46 +1000

    add ability to print cyan

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ Console.ResetColor();
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("==> I'm printing in blue");
 Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("==> I'm printing in cyan");
+Console.ResetColor();
